4229233e0512f9c86913b104a145082e7ad4040b
Salt <amira9madiyar@gmail.com>
2025-02-18 22:02:55 +0500

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <main>
+      <h1 className="p-10 my-5 bg-lime-200 text-neutral-950 text-xl hover:bg-lime-400">Hello world</h1>
+      <Link href="/users">Users</Link>
+    </main>
+  );
+}
