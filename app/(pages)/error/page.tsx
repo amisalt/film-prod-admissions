6d7137a4ab53ae29e22d0c380865413e8ac6fa4e
Salt <amira9madiyar@gmail.com>
2025-03-16 09:20:40 +0500

    error with suspense component solved

--- app/(pages)/error/page.tsx
+++ app/(pages)/error/page.tsx
@@ -1,12 +1,16 @@
 'use client'
 import { useSearchParams } from "next/navigation"
+import { Suspense } from "react"
+
 
 export default function Error() {
   const queryPaarams = useSearchParams()
   return (
     <div>
-      <h1>ERROR {queryPaarams.get("code")} - {queryPaarams.get("error")}</h1>
-      <p>{queryPaarams.get("message")}</p>
+      <Suspense>
+        <h1>ERROR {queryPaarams.get("code")} - {queryPaarams.get("error")}</h1>
+        <p>{queryPaarams.get("message")}</p>
+      </Suspense>
     </div>
   )
 }
