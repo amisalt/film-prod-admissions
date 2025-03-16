be04cd1af5d0f67963e59df5b0a6ed196e7f8e11
Salt <amira9madiyar@gmail.com>
2025-03-16 09:23:19 +0500

    error fully solved

--- app/(pages)/error/page.tsx
+++ app/(pages)/error/page.tsx
@@ -2,14 +2,21 @@
 import { useSearchParams } from "next/navigation"
 import { Suspense } from "react"
 
+function DisplayError(){
+  const queryPaarams = useSearchParams()
+  return (
+    <>
+    <h1>ERROR {queryPaarams.get("code")} - {queryPaarams.get("error")}</h1>
+    <p>{queryPaarams.get("message")}</p>
+    </>
+  )
+}
 
 export default function Error() {
-  const queryPaarams = useSearchParams()
   return (
     <div>
       <Suspense>
-        <h1>ERROR {queryPaarams.get("code")} - {queryPaarams.get("error")}</h1>
-        <p>{queryPaarams.get("message")}</p>
+        <DisplayError/>
       </Suspense>
     </div>
   )
